46d70b2cc9095ec384e55aad790ec006bb7ea619
krzm <kmazanek@gmail.com>
2022-06-18 22:38:47 +0200

    Sync with libs

--- CLI.App.Template/Program.cs
+++ CLI.App.Template/Program.cs
@@ -1,4 +1,15 @@
-﻿using Modern.CLI.App.Template;
+﻿using Config.Wrapper;
+using DIHelper;
+using Modern.CLI.App.Template;
+using Unity;
 
-var app = new CliApp();
-app.Run(args);
+var unity = new UnityContainer()
+    .AddExtension(new Diagnostic());
+var serviceSuite = new ServiceSuite(unity);
+serviceSuite.Register();
+var suite = new SuiteConfig(
+    unity.Resolve<IConfigReader>())
+        .GetSuite(unity);
+IBootstraper booter = new Bootstraper(suite);
+booter.CreateApp();
+booter.RunApp(args);
